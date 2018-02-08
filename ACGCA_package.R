ba1dc4698563cb9d6d0ff49de55d77e4d4b6ad8d
Michael Fell <michael.fell@nau.edu>
2018-02-08 08:50:43 -0700

    Got initial C code to compile working on gsl

--- ACGCA_package.R
+++ ACGCA_package.R
@@ -7,10 +7,11 @@
 ###############################################################################
 
 install.packages("devtools")
-devtools::install_github("klutometis/roxygem")
+devtools::install_github("klutometis/roxygen")
 
 # Load the development library and roxygen which helps with documentation.
 library("devtools")
+options(devtools.install.args = "--no-multiarch")  
 library("roxygen2")
 
 # A devtools function that produces a barebones folder for a package
@@ -19,14 +20,18 @@ create("ACGCA")
 # I added documentation this moves into the ACGCA folder to create
 # documentation.
 setwd("./ACGCA")
+use_rcpp()
 document()
 
 setwd("..")
 install("ACGCA")
 help(package="ACGCA")
+library(ACGCA)
 
 # Test the scafold to make sure it runs. This call is for development and
 # would in no way work with the full funciton. That should be tested using 
 # inputs from the origional paper by Ogle and Pacala (2009).
-ACGCArun(as.matrix(c(1, 1, 1)), as.matrix(c(1,1,1)), 1)
+
+#ACGCArun(as.matrix(c(1, 1, 1)), as.matrix(c(1,1,1)), 1)
+dyn.load("ACGCA.dll")
 
