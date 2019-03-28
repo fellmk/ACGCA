02e11cdf14d51b237dc7b9abfeb7c6248bf4dfc7
Michael Fell <michael.fell@nau.edu>
2019-03-28 10:29:45 -0700

    small changes to descriptions.

--- ACGCA/R/ACGCA-package.R
+++ ACGCA/R/ACGCA-package.R
@@ -7,26 +7,6 @@
 #' demonstration of the functions use is available in the vignette accompanying
 #' this package.
 #' 
-#' Compiling from source:
-#' If you try to compile this package from source be aware that the gsl is 
-#' required. It needs to be installed into a directory R can find or the code
-#' will cause an error. In Windows the gsl can be manually compiled to a 
-#' directory r searches at the time of compilation using mingw or another tool
-#' with make. To compile the gsl from source run the following commands in the
-#' directory with the gsl source code:
-#'
-#'  ./configure --prefix=PATH
-#'  
-#' make
-#' 
-#' make install
-#' 
-#' PATH above should be replaced with the path to the directory R will search
-#' at the time of compilation. If the gsl is installed to an appropriate 
-#' location compilation from source is possible in windows. In Linux 
-#' compilation works as long as the gsl has been installed. In Ubuntu or any 
-#' variant of it this can be accomplished via the package manager.
-#' 
 #' @docType package
 #' @name ACGCA-package
 NULL
